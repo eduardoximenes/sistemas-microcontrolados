38539d460d1a383058cee48cfa27c7b9f5a96635
Eduardo Ximenes <duduximenes470@gmail.com>
2024-03-23 19:15:38 -0300

    feat: insertion sort

--- ap0/eduardoximenes_luispassoni_joaogoulart_ap0/Projeto Exemplo/exemplo.s
+++ ap0/eduardoximenes_luispassoni_joaogoulart_ap0/Projeto Exemplo/exemplo.s
@@ -8,11 +8,15 @@
 ; -------------------------------------------------------------------------------
 ; Declarações EQU - Defines
 ;<NOME>         EQU <VALOR>
-Rdm				EQU 0x20000400
-Fib 			EQU	0x20000500
+INITIAL_LIST EQU 0x20000400 
+FIBONACCI_LIST EQU 0x20000500
+
 ; -------------------------------------------------------------------------------
 ; Área de Dados - Declarações de variáveis
 		AREA  DATA, ALIGN=2
+	
+num_vec SPACE 23
+			
 		; Se alguma variável for chamada em outro arquivo
 		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
 		                                   ; partir de outro arquivo
@@ -35,89 +39,145 @@ Fib 			EQU	0x20000500
 
 ; -------------------------------------------------------------------------------
 ; Função main()
-	
 Start  
 ; Comece o código aqui <======================================================
 
-	;2) Carregar a lista de números aleatória
-	;{3, 244, 14, 233, 1, 6, 9, 18, 13, 254, 21, 34, 2, 67, 135, 8, 89, 43, 5, 105, 144, 201, 55}
-	LDR R0, =Rdm
-	
-	MOV R1, #3
-	STRB R1, [R0], #4 
-	MOV R1, #244
-	STRB R1, [R0], #4 
-	MOV R1, #14
-	STRB R1, [R0], #4 
-	MOV R1, #233
-	STRB R1, [R0], #4 
-	MOV R1, #1
-	STRB R1, [R0], #4 
-	MOV R1, #6
-	STRB R1, [R0], #4 
-	MOV R1, #9
-	STRB R1, [R0], #4 
-	MOV R1, #18
-	STRB R1, [R0], #4 
-	MOV R1, #13
-	STRB R1, [R0], #4 
-	MOV R1, #254
-	STRB R1, [R0], #4 
-	MOV R1, #21
-	STRB R1, [R0], #4 
-	MOV R1, #34
-	STRB R1, [R0], #4 
-	MOV R1, #2
-	STRB R1, [R0], #4 
-	MOV R1, #67
-	STRB R1, [R0], #4 
-	MOV R1, #135
-	STRB R1, [R0], #4 
-	MOV R1, #8
-	STRB R1, [R0], #4 
-	MOV R1, #89
-	STRB R1, [R0], #4 
-	MOV R1, #43
-	STRB R1, [R0], #4 
-	MOV R1, #5
-	STRB R1, [R0], #4 
-	MOV R1, #105
-	STRB R1, [R0], #4 
-	MOV R1, #144
-	STRB R1, [R0], #4 
-	MOV R1, #201
-	STRB R1, [R0], #4 
-	MOV R1, #55
-	STRB R1, [R0] 
-	
-	MOV R10, R0 	; POSICAO FINAL
-	LDR R0, =Rdm 	; POSICAO INICIAL
+; 3, 244, 14, 233, 1, 6, 9, 18, 13, 254, 21, 34, 2, 67, 135,  8, 89, 43, 5, 105, 144, 201, 55
 
-	LDRB R1, [R0], #4
-	LDRB R2, [R0], #4
-	CMP R1, R2
-	BHS 
-	
+	LDR R0, =INITIAL_LIST
+	LDR R1, =FIBONACCI_LIST
+	MOV R9, #0;		STORES FIBONACCI LIST SIZE
 
+	MOV R2, #3
+	STRB R2, [R0], #1
+	MOV R2, #244
+	STRB R2, [R0], #1
+	MOV R2, #14
+	STRB R2, [R0], #1
+	MOV R2, #233
+	STRB R2, [R0], #1
+	MOV R2, #1
+	STRB R2, [R0], #1
+	MOV R2, #6
+	STRB R2, [R0], #1
+	MOV R2, #9
+	STRB R2, [R0], #1
+	MOV R2, #18
+	STRB R2, [R0], #1
+	MOV R2, #13
+	STRB R2, [R0], #1
+	MOV R2, #254
+	STRB R2, [R0], #1
+	MOV R2, #21
+	STRB R2, [R0], #1
+	MOV R2, #34
+	STRB R2, [R0], #1
+	MOV R2, #2
+	STRB R2, [R0], #1
+	MOV R2, #67
+	STRB R2, [R0], #1
+	MOV R2, #135
+	STRB R2, [R0], #1
+	MOV R2, #8
+	STRB R2, [R0], #1
+	MOV R2, #89
+	STRB R2, [R0], #1
+	MOV R2, #43
+	STRB R2, [R0], #1
+	MOV R2, #5
+	STRB R2, [R0], #1
+	MOV R2, #105
+	STRB R2, [R0], #1
+	MOV R2, #144
+	STRB R2, [R0], #1
+	MOV R2, #201
+	STRB R2, [R0], #1
+	MOV R2, #55
+	STRB R2, [R0], #1
+	
+	MOV R6, R0
+	LDR R0, =INITIAL_LIST;	Sets R0 pointer to head of list 
+	
+main_loop
+	LDRB R3, [R0];		Select next element on the list, loads to R3
+	MOV R10, #1;
+	MOV R11, #1;		Fibonacci initial values
+	CMP R6, R0;			Checks if list is over (TODO), comparing where the last number was stored
+	BEQ sorting;		Branch to sorting algorithm
+	ADD R0, #1; 		Increase to get the next element on list
 	
 	
+fibonacci
+	CMP R10, R3;			Checks if value is in fibonacci list
+	BEQ on_list;			Branch if true
+	BHI main_loop;			Branch back to next element if fibonacci is bigger than current value
+	MOV R7, R11;			Aux register
+	MOV R11, R10			
+	ADD R10, R10, R7;		Fibonacci iteration
+	B fibonacci
+	
+on_list
+	STRB R3, [R1], #1;		Value belongs to fibonacci list, stores it
+	ADD R9, R9, #1;			Count total numbers that are in fibonacci
+	
+	B main_loop;			Branch back to next element
+	
+sorting
+	LDR R8, =FIBONACCI_LIST
+	LDR R2, =FIBONACCI_LIST;	R2 will act as a cursor
+	ADD R1, R2, #1;				R1 will point to the current key, starting in the 2nd element
+	MOV R11, R1
 
-
-	;3) Fazer uma varredura da lista de números aleatórios para encontrar quais números são pertencentes à série de Fibonacci,
-	LDR R0, =Rdm
-	LDR R1, =Fib
-label
-	LDRB R2, [R0], #4 
-	CMP 
-
-fib
+	SUB R9, #1; 			    Increment R9 so it covers the last element 
 	
-	
+insertion_sort
+	LDRB R3, [R1];			Select key element
+	LDRB R4, [R2];   		Element pointed by cursor
+ 		
+	B compare	
+		
+next_element
+	MOV R1, R11;			Recovers previous key
+	ADD R1, R1, #1; 		Moves key to next element
+	MOV R11, R1;			Saves current key
+	SUB R2, R1, #1;
 
+    SUB R9, #1;				Decrement R9 until the list is over			
+	CMP R9, #0
+	IT EQ
+		BEQ	finish;
 	
+	B insertion_sort
+	
+compare
+	CMP R4, R3;         Check R4 > R3
+	BLHI swap;          If true swap
+	
+	SUB R10, R8, R2
+	CMP	R10, #0				
+	BNE move_cursor;	Branch if cursor (R2) is not pointing to head of list
+	
+	B next_element;     Go to next element
+	
+swap
+	STRB R4, [R1]
+    STRB R3, [R2];          Swap R3 and R4
+	
+	SUB R10, R2, R8;	
+	CMP	R10, #0
+	ITTE NE;				If cursor (R2) is not pointing to first element of list
+		MOVNE R1, R2;		Then: R1 follows the key element
+		SUBNE R2, R1, #1;	Then: Cursor goes to element behind key
+		BEQ next_element;   Else: Go to next element
+	
+	B insertion_sort
+	
+move_cursor
+	SUB R2, R2, #1; 		Moves cursor to previous index
+	B insertion_sort;		Branch back to loop
+	
+finish    
 	NOP
-
-
 
     ALIGN                           ; garante que o fim da seção está alinhada 
     END                             ; fim do arquivo
